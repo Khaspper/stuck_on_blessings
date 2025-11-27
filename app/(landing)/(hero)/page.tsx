e550f61494ae2fd6911fb5b8d36f9eabfc65f65b
Mark <brandonnarciso24@gmail.com>
2025-11-26 23:02:13 -0800

    fix: change CSS units

--- app/(landing)/(hero)/page.tsx
+++ app/(landing)/(hero)/page.tsx
@@ -11,37 +11,42 @@ export default function Hero() {
   );
 }
 
-// TODO: Change all the text-9xl etc to something like this
-// TODO: text-[20vw]
-// TODO: w-[30vw]
-// TODO: gap-[3vw]
-
 export function HeroMobile() {
   return (
     <div className="sm:hidden flex flex-col items-center">
       <InfiniteScrollingLogosAnimation />
-      <div className="flex items-center px-4 pt-16 pb-4 gap-4">
+      <div className="flex items-center px-4 pt-16 pb-4 gap-[5vw]">
         {/* LEFT TEXT */}
         <div className="flex flex-col">
-          <div className="flex gap-8">
-            <span className="text-9xl leading-[.9]">A</span>
-            <span className="text-9xl leading-[.9]">FAITH</span>
+          <div className="flex gap-[8vw]">
+            <span className="text-[clamp(5rem,30vw,10rem)]  leading-[.9]">
+              A
+            </span>
+            <span className="text-[clamp(5rem,30vw,10rem)] leading-[.9]">
+              FAITH
+            </span>
           </div>
-          <span className="text-9xl leading-[.9]">THAT</span>
-          <span className="text-9xl leading-[.9]">STICKS</span>
+          <span className="text-[clamp(5rem,30vw,10rem)] leading-[.9]">
+            THAT
+          </span>
+          <span className="text-[clamp(5rem,30vw,10rem)] leading-[.9]">
+            STICKS
+          </span>
         </div>
         {/* RIGHT IMAGE */}
         <Image
           src={phone}
           alt="Cell Phone sticker"
-          className="w-[110px] h-auto shrink-0 -mb-24 -ml-10"
+          className="w-[clamp(4rem,25vw,10rem)] h-auto shrink-0 -mb-24 -ml-10"
         />
       </div>
       <div>
-        <p className="text-center text-2xl">
+        <p className="text-center text-[clamp(1rem,5vw,10rem)]">
           Commit your way to the Lord... trust in Him
         </p>
-        <p className="text-center text-2xl">and He will act. - Psalms 37:5</p>
+        <p className="text-center text-[clamp(1rem,5vw,10rem)]">
+          and He will act. - Psalms 37:5
+        </p>
       </div>
     </div>
   );
